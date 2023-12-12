c274b1f628e372570c306d62cd636a1bba71f844
ttbkk <ttbkk@TBNMB00017.local>
2023-12-12 13:55:39 +0700

    add onchange format number

--- src/TbnCustomInput.jsx
+++ src/TbnCustomInput.jsx
@@ -1,11 +1,28 @@
 import { createElement } from "react";
 
 import "./ui/TbnCustomInput.css";
-import { Input, Tooltip } from "antd";
+import { Input } from "antd";
 
-export function TbnCustomInput({ prefix, suffix, textAttribute, placeHolder }) {
+export function TbnCustomInput({ prefix, suffix, textAttribute, placeHolder, disabled, onChangeAct }) {
+    function onChange(value) {
+        if (onChangeAct && onChangeAct.canExecute && !onChangeAct.isExecuting) {
+            onChangeAct.execute(value);
+        }
+    }
+
     if (textAttribute.status === "available") {
-        return <Input prefix={prefix} suffix={suffix} value={textAttribute.value} placeholder={placeHolder} />;
+        return (
+            <Input
+                prefix={prefix}
+                suffix={suffix}
+                value={textAttribute.value}
+                placeholder={placeHolder}
+                formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                parser={value => value.replace(/\$\s?|(,*)/g, "")}
+                onChange={onChange}
+                disabled={disabled}
+            />
+        );
     } else {
         return <div></div>;
     }
